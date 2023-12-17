b58f0d79c31bb43be4aa415b8ec3dd8b79be4875
sdcharly <142165510+sdcharly@users.noreply.github.com>
2023-12-18 00:28:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,64 +24,86 @@ logging.basicConfig(level=logging.INFO)
 openai.api_key = os.getenv('OPENAI_API_KEY')
 genai.configure(api_key=os.getenv('GEMINI_KEY'))
 
+global_model = None
+
 # Function to check allowed file types
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-    
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
-    if 'image' not in request.files:
-        return jsonify({"error": "No image part"}), 400
-    file = request.files['image']
-    if file.filename == '' or not allowed_file(file.filename):
-        return jsonify({"error": "No selected image or file type not allowed"}), 400
+    try:
+        if 'image' not in request.files:
+            return jsonify({"error": "No image part"}), 400
+        file = request.files['image']
+        if file.filename == '' or not allowed_file(file.filename):
+            return jsonify({"error": "No selected image or file type not allowed"}), 400
 
-    filename = secure_filename(file.filename)
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(file_path)
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
 
-    embedding = process_image(file_path)
-    upsert_to_pinecone(filename, embedding)
+        embedding = process_image(file_path)
+        upsert_to_pinecone(filename, embedding)
 
-    return jsonify({"message": "Image uploaded successfully"})
+        return jsonify({"message": "Image uploaded successfully"})
+    except Exception as e:
+        logging.error(f"Upload Image Error: {e}")
+        return jsonify({"error": str(e)}), 500
 
-# Initialize Pinecone
-def init_pinecone():
-    pinecone.init(api_key=os.getenv('PINECONE_API_KEY'), environment='gcp-starter')
-    if 'imgcompare' not in pinecone.list_indexes():
-        pinecone.create_index('imgcompare', dimension=1536)
+def init_genai_model():
+    global global_model
+    global_model = genai.GenerativeModel(
+        # Model configuration
+    )
+
+# Initialize the model when the app starts
+init_genai_model()
 
 INDEX_NAME = 'imgcompare'
+
+def init_pinecone():
+    try:
+        pinecone.init(api_key=os.getenv('PINECONE_API_KEY'), environment='gcp-starter')
+        if 'imgcompare' not in pinecone.list_indexes():
+            pinecone.create_index('imgcompare', dimension=1536)
+    except Exception as e:
+        logging.error(f"Pinecone Initialization Error: {e}")
+        raise
+
 init_pinecone()
 
-# Function to upsert embeddings to Pinecone
 def upsert_to_pinecone(image_name, embedding):
-    index = pinecone.Index(INDEX_NAME)
-    data = {image_name: embedding.tolist()}
     try:
+        index = pinecone.Index(INDEX_NAME)
+        data = {image_name: embedding.tolist()}
         index.upsert(vectors=data)
         logging.info(f"Upserted {image_name} to Pinecone successfully.")
     except Exception as e:
         logging.error(f"Error upserting to Pinecone: {e}")
 
 def process_image(image_path):
-    with open(image_path, 'rb') as image_file:
-        image_data = image_file.read()
+    try:
+        with open(image_path, 'rb') as image_file:
+            image_data = image_file.read()
 
-    image_prompt = {"mime_type": "image/jpeg", "data": image_data}
-    input_prompt = "You are an expert in identifying images and objects in the image and describing them."
-    question = "Describe this image:"
+        image_prompt = {"mime_type": "image/jpeg", "data": image_data}
+        input_prompt = "You are an expert in identifying images and objects in the image and describing them."
+        question = "Describe this image:"
 
-    prompt_parts = [input_prompt, image_prompt, question]
-    response = model.generate_content(prompt_parts)
+        prompt_parts = [input_prompt, image_prompt, question]
+        response = global_model.generate_content(prompt_parts)
 
-    description = response.text  # Modify based on actual response structure
-    embedding = generate_embedding_with_description(description)
-    return embedding
+        description = response.text  # Modify based on actual response structure
+        embedding = generate_embedding_with_description(description)
+        return embedding
+    except Exception as e:
+        logging.error(f"Process Image Error: {e}")
+        raise
 
 def generate_embedding_with_description(description):
     try:
