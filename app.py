da46401c376bda79a400b35c37e1acabbee77e3c
sdcharly <142165510+sdcharly@users.noreply.github.com>
2023-12-17 13:10:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-import os openai pinecone
+import os
+import openai
+import pinecone
+import logging
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-UPLOAD_FOLDER = ''
+UPLOAD_FOLDER = ''  # Update with a valid path
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -16,6 +15,10 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
@@ -26,31 +29,20 @@ def upload_image():
         return 'No selected image'
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         try:
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return 'Image successfully uploaded'
-        except Exception as e:
-            return str(e)  # Or a more user-friendly message
-    return 'Invalid file type'
-
-    if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
-    
-            # Generate Embedding
             embedding = generate_embedding(file_path)
-    
-            # Upsert to Pinecone
             pinecone_index = init_pinecone()
             pinecone_index.upsert([(filename, embedding)])
-            
             return 'Image successfully uploaded and indexed'
-        return 'Invalid file type'
+        except Exception as e:
+            logging.error(f"Error uploading file: {e}")
+            return 'Error in file processing'
+    return 'Invalid file type'
 
 def generate_embedding(image_path):
     openai.api_key = os.getenv("OPENAI_API_KEY")
-
     with open(image_path, "rb") as image_file:
         image_data = image_file.read()
     
@@ -58,13 +50,12 @@ def generate_embedding(image_path):
         image_data=image_data,
         model="gemini-pro-vision"
     )
-
     return response['data']['embedding']
     
 def init_pinecone():
     pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment='gcp-starter')
-    pinecone.create_index('image_index', dimension=512, metric='cosine')
+    # Consider moving pinecone.create_index out of this function if it's a one-time operation
     return pinecone.Index('image_index')
-    
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)  # Set debug to False for production
