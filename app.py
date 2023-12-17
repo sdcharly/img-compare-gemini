487891f40d87ea8a48cd007cce60887da2b8f45d
sdcharly <142165510+sdcharly@users.noreply.github.com>
2023-12-17 12:02:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+UPLOAD_FOLDER = 'path/to/uploaded/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return 'No image part'
+    file = request.files['image']
+    if file.filename == '':
+        return 'No selected image'
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'Image successfully uploaded'
+    return 'Invalid file type'
 
 if __name__ == '__main__':
     app.run(debug=True)
