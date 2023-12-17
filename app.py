f0a5724508b42258657ac268bc6398b4a8ca484a
sdcharly <142165510+sdcharly@users.noreply.github.com>
2023-12-17 13:25:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import logging
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = ''  # Update with a valid path
+UPLOAD_FOLDER = './uploads/'  # Update with a valid path
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
