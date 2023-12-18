e2366b1baf348f10bb405a0c310c0d8d97638fdf
sdcharly <142165510+sdcharly@users.noreply.github.com>
2023-12-18 15:22:03 +0530

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,57 @@
+// Event listener for the search form submission
+document.getElementById('searchForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    processImageRequest(this, '/search', 'searchResults', true);
+});
+
+// Event listener for the upload form submission
+document.getElementById('uploadForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    processImageRequest(this, '/upsert', 'uploadStatus', false);
+});
+
+// Function to process the image request
+function processImageRequest(form, url, resultContainerId, isSearch) {
+    var formData = new FormData(form);
+    var resultContainer = document.getElementById(resultContainerId);
+    var progressBar = document.querySelector('.progress-bar');
+    var progressDiv = document.querySelector('.progress');
+
+    resultContainer.innerHTML = ''; // Clear previous results or status
+    progressBar.style.width = '0%';
+    progressDiv.style.display = 'block';
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return isSearch ? response.json() : response.text();
+    }).then(data => {
+        progressBar.style.width = '100%';
+        if (isSearch) {
+            displaySearchResults(data, resultContainer);
+        } else {
+            resultContainer.innerHTML = data;
+        }
+    }).catch(error => {
+        console.error('Fetch error:', error);
+        resultContainer.innerText = 'Operation failed. Please try again.';
+    }).finally(() => {
+        setTimeout(() => progressDiv.style.display = 'none', 2000); // Hide progress bar after delay
+    });
+}
+
+// Function to display search results
+function displaySearchResults(data, container) {
+    if (data.results && data.results.length > 0) {
+        var resultsHtml = data.results.map(result => {
+            return `<div><strong>ID:</strong> ${result.id}, <strong>Score:</strong> ${result.score}</div>`;
+        }).join('');
+        container.innerHTML = `<h4>Results:</h4>${resultsHtml}`;
+    } else {
+        container.innerHTML = 'No matches found';
+    }
+}
