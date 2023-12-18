5baf21b75053ccec7b0d8c8c8086b495c3ec706e
sdcharly <142165510+sdcharly@users.noreply.github.com>
2023-12-18 14:09:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ INDEX_NAME = 'imgcompare'
 
 # Set up the generative model configuration
 generation_config = {
-    "temperature": 0.6,
+    "temperature": 0.1,
     "top_p": 1,
     "top_k": 32,
     "max_output_tokens": 4096,
